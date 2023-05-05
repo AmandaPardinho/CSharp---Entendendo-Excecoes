f3b7ab08d528d656ecaf5f175924ea70d5846aec
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-05 18:38:47 -0300

    Criação de um bloco try-catch e mensagens que devem ser exibidas

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,6 +1,14 @@
 ﻿using bytebank.Titular;
 using bytebank.Contas;
 
-ContaCorrente conta1 = new ContaCorrente(283, "1234-X");
-Console.WriteLine(ContaCorrente.TaxaOperacao);
+try
+{
+    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+}
+catch(ArgumentException ex) 
+{
+    Console.WriteLine(ex.ParamName); //mostra o nome do argumento que está lançando a exceção 
+    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero.");
+    Console.WriteLine(ex.Message);
+}
 Console.ReadKey();
